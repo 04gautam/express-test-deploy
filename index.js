23a6f097e7e8ed89c932c692926597224ffc4f4f
Hitlar Gautam <gautamhitlar2@gmail.com>
2025-10-22 13:02:08 +0530

    for deploy

--- index.js
+++ index.js
@@ -36,6 +36,6 @@ app.get("/about", (req, res) => {
 
 
 
-app.listen(3000, ()=>console.log("server is running on port 3000"))
+// app.listen(3000, ()=>console.log("server is running on port 3000"))
 
-// module.exports = app; // 
+module.exports = app; // 
